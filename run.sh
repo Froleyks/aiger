2b263212cfd0ff0e772bb090702be05c1f191c6c
froleyks <nfroleyks@gmail.com>
2024-08-02 15:55:01 +0200

    this doesn't work

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+c=$(realpath $1)
+x=/home/froleyks/24_voiraig/fuzz/voiraig
+cd $(dirname $0)
+echo "testing $c"
+printf "\nnew\n"
+time ./aigdd -r $c new.aag $x
+n=$(wc -l new.aag | awk '{print $1}')
+echo "new: $n"
+# exit 0
+printf "\nold\n"
+time ../olddd -r $c old.aag $x
+o=$(wc -l old.aag | awk '{print $1}')
+echo "old: $o new: $n"
+exit $(($n - $o))
